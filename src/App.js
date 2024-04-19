57c594f8633beaf5324dc17d8b17512a081a719b
Snehesh-Thattil <sneheshthattil75@gmail.com>
2024-04-19 23:26:20 +0530

    [ Items listing down, Finished Section, Design modified : ✅ ]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -6,23 +6,43 @@ function App() {
   let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   let weekDay = weekDays[day]
 
+  const [AllToDo, setAllToDo] = useState([])
+  const [toDo, setToDo] = useState('')
+
   return (
     <div className="app">
-      <h1>ToDo List</h1>
-      <h2>Whoop, it's {weekDay} 🌝 ☕ </h2>
+      <div className="tasks">
+        
+        <h1>ToDo List</h1>
+        <h2>Whoop, it's {weekDay} 🌝 ☕</h2>
 
-      <div className="inputDiv">
-        <i className='fas fa-pen'></i>
-        <input type="text" placeholder="Add item..." />
-        <i className="fas fa-plus"></i>
+        <div className="inputDiv">
+          <i className='fas fa-pen'></i>
+          <input className='one' value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="Add item..." />
+          <i className="fas fa-plus" onClick={() => setAllToDo([...AllToDo, toDo])}></i>
+        </div>
+
+        {AllToDo.map((obj) => {
+          return (
+            <div className="todos">
+              <input type="checkbox" />
+              <p>{obj}</p>
+              <i className="fas fa-times"></i>
+            </div>
+          )
+        })}
+
       </div>
 
-      <div className="todos">
-        <input type="checkbox" name="" id="" />
-        <p>Rect tutorial</p>
-        <i className="fas fa-times"></i>
+      <div className="finished">
+        <h3>Finished Tasks</h3>
+        <div className="items">
+          <i class="fa-solid fa-circle-check"></i>
+          <p>Item</p>
+          <i className='fas fa-times'></i>
+        </div>
       </div>
-      
+
     </div>
   )
 }
