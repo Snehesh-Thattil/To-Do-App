a6cef808b4ae888b0e626bf21bc6f17b75422ff2
Snehesh-Thattil <sneheshthattil75@gmail.com>
2024-04-21 11:36:46 +0530

    [ Media Queries Fixed, Quote Section, Alignment modified : ✅ Finished ]

--- src/App.js
+++ src/App.js
@@ -28,159 +28,166 @@ function App() {
   // XML Starts Here //
   return (
     <div className="app">
-      
+
+      <div className="header">
         <h1>ToDo List</h1>
         <h2>Whoop, it's {weekDay} 🌝 ☕</h2>
+      </div>
 
-        <div className="inputDiv">
-          <i className='fas fa-pen'></i>
-          <input className='inputBar' onChange={(e) => setToDo(e.target.value)} type="text" placeholder="Add item..." />
-          <select name="Priority" onChange={(e) => setPrrty(e.target.value)}>
-            <option value="0" defaultValue hidden>Priority</option>
-            <option value="1">Concentrate</option>
-            <option value="2">Delegate</option>
-            <option value="3">Schedule</option>
-            <option value="4">Ignorable</option>
-          </select>
-          <i className="fas fa-plus" onClick={() => {
-            let inputBar = document.querySelector('.inputBar')
+      <div className="inputDiv">
+        <i className='fas fa-pen'></i>
+        <input className='inputBar' onChange={(e) => setToDo(e.target.value)} type="text" placeholder="Add item..." />
+        <select name="Priority" onChange={(e) => setPrrty(e.target.value)}>
+          <option value="0" defaultValue hidden>Priority</option>
+          <option value="1">Concentrate</option>
+          <option value="2">Delegate</option>
+          <option value="3">Schedule</option>
+          <option value="4">Ignorable</option>
+        </select>
+        <i className="fas fa-plus" onClick={() => {
+          let inputBar = document.querySelector('.inputBar')
 
-            if (prrty < '1') {
-              alert('Choose a Valid Task Priority')
-            } else if (inputBar.value.length < 3) {
-              alert('Enter a Valid Task Name')
-            } else {
-              setAllToDo([...AllToDo, { id: Date.now(), task: toDo, priority: prrty, check: false }])
-              inputBar.value = ""
-              inputBar.focus()
+          if (prrty < '1') {
+            alert('Choose a Valid Task Priority')
+          } else if (inputBar.value.length < 3) {
+            alert('Enter a Valid Task Name')
+          } else {
+            setAllToDo([...AllToDo, { id: Date.now(), task: toDo, priority: prrty, check: false }])
+            inputBar.value = ""
+            inputBar.focus()
+          }
+        }}></i>
+      </div>
+
+      <div className="todos">
+
+        {/* Concentrate Tasks */}
+        <div className="concentrate">
+          <p className="typeHeading">Concentrate Tasks</p>
+          {AllToDo.map((obj) => {
+            if (obj.priority === '1') {
+              return (
+                <div className="todo">
+
+                  <input value={obj.checked} onChange={(e) => {
+                    setAllToDo(AllToDo.filter((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.check = e.target.checked
+                      }
+                      return obj2
+                    }))
+                  }} type="checkbox" />
+
+                  {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
+
+                  <div className="icon" onClick={removeTask}>
+                    <i className="fas fa-times"></i>
+                  </div>
+
+                </div>
+              )
             }
-          }}></i>
+            return null
+          })}
         </div>
 
-        <div className="todos">
+        {/* Delegate Tasks */}
+        <div className="delegate">
+          <p className="typeHeading">Delegate Tasks</p>
+          {AllToDo.map((obj) => {
+            if (obj.priority === '2') {
+              return (
+                <div className="todo">
 
-          {/* Concentrate Tasks */}
-          <div className="concentrate">
-            <p className="typeHeading">Concentrate Tasks</p>
-            {AllToDo.map((obj) => {
-              if (obj.priority === '1') {
-                return (
-                  <div className="todo">
+                  <input value={obj.checked} onChange={(e) => {
+                    setAllToDo(AllToDo.filter((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.check = e.target.checked
+                      }
+                      return obj2
+                    }))
+                  }} type="checkbox" />
 
-                    <input value={obj.checked} onChange={(e) => {
-                      setAllToDo(AllToDo.filter((obj2) => {
-                        if (obj2.id === obj.id) {
-                          obj2.check = e.target.checked
-                        }
-                        return obj2
-                      }))
-                    }} type="checkbox" />
+                  {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
 
-                    {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
-
-                    <div className="icon" onClick={removeTask}>
-                      <i className="fas fa-times"></i>
-                    </div>
-
+                  <div className="icon" onClick={removeTask}>
+                    <i className="fas fa-times"></i>
                   </div>
-                )
-              }
-              return null
-            })}
-          </div>
 
-          {/* Delegate Tasks */}
-          <div className="delegate">
-            <p className="typeHeading">Delegate Tasks</p>
-            {AllToDo.map((obj) => {
-              if (obj.priority === '2') {
-                return (
-                  <div className="todo">
-
-                    <input value={obj.checked} onChange={(e) => {
-                      setAllToDo(AllToDo.filter((obj2) => {
-                        if (obj2.id === obj.id) {
-                          obj2.check = e.target.checked
-                        }
-                        return obj2
-                      }))
-                    }} type="checkbox" />
-
-                    {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
-
-                    <div className="icon" onClick={removeTask}>
-                      <i className="fas fa-times"></i>
-                    </div>
-
-                  </div>
-                )
-              }
-              return null
-            })}
-          </div>
-
-          {/* Schedule Tasks */}
-          <div className="schedule">
-            <p className="typeHeading">Schedule Tasks</p>
-            {AllToDo.map((obj) => {
-              if (obj.priority === '3') {
-                return (
-                  <div className="todo">
-
-                    <input value={obj.checked} onChange={(e) => {
-                      setAllToDo(AllToDo.filter((obj2) => {
-                        if (obj2.id === obj.id) {
-                          obj2.check = e.target.checked
-                        }
-                        return obj2
-                      }))
-                    }} type="checkbox" />
-
-                    {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
-
-                    <div className="icon" onClick={removeTask}>
-                      <i className="fas fa-times"></i>
-                    </div>
-
-                  </div>
-                )
-              }
-              return null
-            })}
-          </div>
-
-          {/* Ignorable Tasks */}
-          <div className="ignorable">
-            <p className="typeHeading">Ignorable Tasks</p>
-            {AllToDo.map((obj) => {
-              if (obj.priority === '4') {
-                return (
-                  <div className="todo">
-
-                    <input value={obj.checked} onChange={(e) => {
-                      setAllToDo(AllToDo.filter((obj2) => {
-                        if (obj2.id === obj.id) {
-                          obj2.check = e.target.checked
-                        }
-                        return obj2
-                      }))
-                    }} type="checkbox" />
-
-                    {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
-
-                    <div className="icon" onClick={removeTask}>
-                      <i className="fas fa-times"></i>
-                    </div>
-
-                  </div>
-                )
-              }
-              return null
-            })}
-          </div>
-
+                </div>
+              )
+            }
+            return null
+          })}
         </div>
-        
+
+        {/* Schedule Tasks */}
+        <div className="schedule">
+          <p className="typeHeading">Schedule Tasks</p>
+          {AllToDo.map((obj) => {
+            if (obj.priority === '3') {
+              return (
+                <div className="todo">
+
+                  <input value={obj.checked} onChange={(e) => {
+                    setAllToDo(AllToDo.filter((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.check = e.target.checked
+                      }
+                      return obj2
+                    }))
+                  }} type="checkbox" />
+
+                  {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
+
+                  <div className="icon" onClick={removeTask}>
+                    <i className="fas fa-times"></i>
+                  </div>
+
+                </div>
+              )
+            }
+            return null
+          })}
+        </div>
+
+        {/* Ignorable Tasks */}
+        <div className="ignorable">
+          <p className="typeHeading">Ignorable Tasks</p>
+          {AllToDo.map((obj) => {
+            if (obj.priority === '4') {
+              return (
+                <div className="todo">
+
+                  <input value={obj.checked} onChange={(e) => {
+                    setAllToDo(AllToDo.filter((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.check = e.target.checked
+                      }
+                      return obj2
+                    }))
+                  }} type="checkbox" />
+
+                  {obj.check ? <p><s>{obj.task}</s></p> : <p>{obj.task}</p>}
+
+                  <div className="icon" onClick={removeTask}>
+                    <i className="fas fa-times"></i>
+                  </div>
+
+                </div>
+              )
+            }
+            return null
+          })}
+        </div>
+
+      </div>
+
+      <div className="quote">
+        <h4>"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time."<br />
+          <span>- Thomas Edison</span></h4>
+      </div>
+
     </div>
   )
 }
